735de986539e432983b001db172b57b58336c83d
pking <pking@xojetaviation.com>
2022-12-23 14:53:21 -0500

    fix tests | pass in auth header

--- tests/route_test.py
+++ tests/route_test.py
@@ -1,6 +1,16 @@
 import os
 import json
 import requests
+import pytest
+
+
+def access_token():
+    username = "kingster307"
+    pw = "kingster_test"
+    requests.get("http://localhost:3001/login", params={"username": username, "pw": pw})
+    token = requests.get("http://localhost:3001/access_token", {})
+    requests.get("http://localhost:3001/logout", {})
+    return token
 
 def format_res(resp) -> dict:
     resp_dict = {
@@ -22,7 +32,7 @@ def get_pulumi_output():
         return json.load(read_file)
 
 def test_test():
-    res = format_res(requests.get(f"{get_pulumi_output()['endpoint']}/just_do_it"))
+    res = format_res(requests.get(f"{get_pulumi_output()['endpoint']}/just_do_it", headers={"Authorization": f"Bearer {access_token()}"}))
     print(res)
     assert res["status"] == 200
     assert res["content"].decode("utf-8")  == "hello"
